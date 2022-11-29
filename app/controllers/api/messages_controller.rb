05998c7cc9899f64e219ef186e343a273f480838
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2022-11-29 19:44:37 +0000

    Add linters configuration

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,6 +2,6 @@ class Api::MessagesController < ApplicationController
   # GET /messages or /messages.json
   def index
     @message = Message.all.sample
-    render :json => @message
+    render json: @message
   end
 end
